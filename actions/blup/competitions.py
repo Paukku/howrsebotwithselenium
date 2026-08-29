0c632064465978fd390349eae66f23955321750e
Paukku1 <paula.hayrynen@hotmail.fi>
2026-08-29 22:38:20 +0300

    Added all competitions

--- actions/blup/competitions.py
+++ actions/blup/competitions.py
@@ -2,33 +2,107 @@ from selenium.webdriver.common.by import By
 from utils.randomTime import sleep
 from actions.care.care_actions import element_exists
 
-def jumping_competition(driver, amount):
+def competition(driver, amount, selectors, name):
   for i in range(amount):
-    print(f"Estekisa {i + 1}/{amount}")
+    print(f"{name} {i + 1}/{amount}")
 
-    driver.find_element(
-      By.CSS_SELECTOR,
-      "a.competition-saut"
-    ).click()
+    for selector in selectors:
+      if element_exists(driver, By.CSS_SELECTOR, selector):
+        driver.find_element(
+          By.CSS_SELECTOR,
+          selector
+        ).click()
+        break
+    else:
+      raise Exception(f"{name}-kilpailua ei löytynyt")
+
     sleep()
+
+# Classic
+def jumping_competition(driver, amount):
+  competition(
+    driver,
+    amount,
+    ["a.competition-saut"],
+    "Estekisa"
+  )
+
+def cross_competition(driver, amount):
+  competition(
+    driver,
+    amount,
+    ["a.competition-cross"],
+    "Maastokisa"
+  )
 
 def dressage_competition(driver, amount):
-  for i in range(amount):
-    print(f"koulukisa {i + 1}/{amount}")
+  competition(
+    driver,
+    amount,
+    [
+      "a.competition-dressage-rainbow",
+      "a.competition-dressage"
+    ],
+    "Koulukisa"
+  )
 
-    if element_exists(
-      driver,
-      By.CSS_SELECTOR,
-      "a.competition-dressage-rainbow"
-    ):
-      driver.find_element(
-        By.CSS_SELECTOR,
-        "a.competition-dressage-rainbow"
-      ).click()
-    else:
-      driver.find_element(
-        By.CSS_SELECTOR,
-        "a.competition-dressage"
-      ).click()
+def trot_competition(driver, amount):
+  competition(
+    driver,
+    amount,
+    ["a.competition-trot"],
+    "Ravikisa"
+  )
 
-    sleep()
+def galop_competition(driver, amount):
+  competition(
+    driver,
+    amount,
+    ["a.competition-galop"],
+    "Laukkakisa"
+  )
+
+# Western
+# Classic
+def barrel_competition(driver, amount):
+  competition(
+    driver,
+    amount,
+    ["a.competition-barrel"],
+    "Tynnyrikisa"
+  )
+
+def cutting_competition(driver, amount):
+  competition(
+    driver,
+    amount,
+    ["a.competition-cutting"],
+    "Cutting"
+  )
+
+def trail_competition(driver, amount):
+  competition(
+    driver,
+    amount,
+    [
+      "a.competition-trail-class-rainbow",
+      "a.competition-trail-class"
+    ],
+    "Trail"
+  )
+
+def reining_competition(driver, amount):
+  competition(
+    driver,
+    amount,
+    ["a.competition-reining"],
+    "Reining"
+  )
+
+def western_pleasure_competition(driver, amount):
+  competition(
+    driver,
+    amount,
+    ["a.competition-western-pleasure"],
+    "Western pleasure"
+  )
